4a0eb6e28c7d7af3a913822302e3038068a9f605
tremlab <tremlab@icloud.com>
2017-06-04 20:04:47 +0000

    recursive approach to math generator.

--- Recursion/math_exp_generator.py
+++ Recursion/math_exp_generator.py
@@ -8,41 +8,89 @@ with random numbers and random operators. output is string format.
 import random
 
 
-def start_exp():
-    # must start with num, not operator
-    output_list = []
+def start_exp_r():
+    iterations = random.randint(1, 5)
+    exp = exp_recursion(iterations)
+    exp.append(str(random.randint(0, 9)))
+    ### TODO check list and close last paren
+    return " ".join(exp)
 
+
+def exp_recursion(iterations, count=0, open_paren=0):
+
+    if iterations == count:
+        return ""
+
+    operators = ["+", "-", "/", "%", "*"]
+    op_index = random.randint(0, 3)
+    operator = operators[op_index]
+
+    num = str(random.randint(0, 15))
+
+    exp = []
+
+    if open_paren:
+        close_paren = random.randint(0, 1)
+        if close_paren:
+            exp = [num, ")", operator]
+            open_paren = 0
+    else:
+        open_paren = random.randint(0, 1)
+        if open_paren:
+            exp = ["(", num, operator]
+        else:
+            exp = [num, operator]
+    
+    exp.extend(exp_recursion(iterations, count + 1, open_paren))
+
+    return exp
+
+
+def exp_iteration():
+
+    # starts the string - maybe a paren, definitely a num.
+    output_list = []
     open_paren = random.randint(0, 1)
     if open_paren:
         output_list.append("(")
-        output_list.append(start_exp())
-        output_list.append(")")
-    else:
-        output_list.append(str(random.randint(0, 9)))
+    output_list.append(str(random.randint(0, 9)))
 
-    iterations = random.randint(1, 5)
+    iterations = random.randint(1, 15)
 
     for i in range(iterations):
-        exp = build_exp()
+        exp = op_and_num()
+
+        if open_paren:
+            close_paren = random.randint(0, 1)
+            if close_paren:
+                exp += (")")
+                open_paren = 0
+        else:
+            open_paren = random.randint(0, 1)
+            if open_paren:
+                exp = exp[0] + " ( " + exp[-1]
+
         output_list.append(exp)
+
+    if open_paren:
+        output_list.append(")")
 
     return " ".join(output_list)
 
 
-def build_exp():
+def op_and_num():
     operators = ["+", "-", "/", "%", "*"]
 
     op_index = random.randint(0, 3)
     operator = operators[op_index]
     num = str(random.randint(0, 9))
 
-    return " " + operator + " " + num
+    return operator + " " + num
 
 
-print start_exp()
-print start_exp()
-print start_exp()
-print start_exp()
+print start_exp_r()
+print exp_iteration()
 
-# not perfect.  Either the exp starts with paren, or there are NO PAREN in the
-# whole exp.  :(
+
+## ideally, I'd like to ahve the option of NESTED parens. :(
+## but for now, this solution works well.
