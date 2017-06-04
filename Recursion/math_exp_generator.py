2c911f0e8bc05990fdb6714e1b08cfba71b57165
tremlab <tremlab@icloud.com>
2017-06-04 23:21:22 +0000

    closing last open paren.

--- Recursion/math_exp_generator.py
+++ Recursion/math_exp_generator.py
@@ -9,16 +9,23 @@ import random
 
 
 def start_exp_r():
-    iterations = random.randint(1, 5)
+    iterations = random.randint(1, 20)
+
     exp = exp_recursion(iterations)
-    exp.append(str(random.randint(0, 9)))
-    ### TODO check list and close last paren
+
+    final_num = str(random.randint(0, 9))
+    if exp[-1] == ")":
+        exp.insert(-1, final_num)
+    else:
+        exp.append(final_num)
     return " ".join(exp)
 
 
 def exp_recursion(iterations, count=0, open_paren=0):
 
     if iterations == count:
+        if open_paren:
+            return ")"
         return ""
 
     operators = ["+", "-", "/", "%", "*"]
@@ -40,7 +47,7 @@ def exp_recursion(iterations, count=0, open_paren=0):
             exp = ["(", num, operator]
         else:
             exp = [num, operator]
-    
+
     exp.extend(exp_recursion(iterations, count + 1, open_paren))
 
     return exp
@@ -89,7 +96,7 @@ def op_and_num():
 
 
 print start_exp_r()
-print exp_iteration()
+# print exp_iteration()
 
 
 ## ideally, I'd like to ahve the option of NESTED parens. :(
